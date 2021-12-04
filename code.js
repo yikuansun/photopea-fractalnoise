eac6df7df38a2050bc1184323dc09de7c9807020
yikuansun <yikuansun2015@gmail.com>
2021-12-04 11:45:00 -0500

    Add to document

--- code.js
+++ code.js
@@ -16,3 +16,29 @@ for (var inputElem of document.querySelectorAll("input, select")) {
         document.querySelector("svg").setAttribute("viewBox", `0 0 ${w} ${h}`);
     }
 )();
+
+document.querySelector("button").addEventListener("click", function() {
+    async function rasterize(svgElem, quality=1, format="png") {
+        var svgData = new XMLSerializer().serializeToString(svgElem);
+        var imgElem = document.createElement("img");
+        imgElem.src = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData)));
+        const myPromise = new Promise((resolve, reject) => {
+            imgElem.onload = function() {
+                var svgClientRect = {
+                    width: parseFloat(svgElem.getAttribute("width")),
+                    height: parseFloat(svgElem.getAttribute("height"))
+                };
+                var canvas = document.createElement("canvas");
+                canvas.width = svgClientRect.width;
+                canvas.height = svgClientRect.height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(imgElem, 0, 0, svgClientRect.width, svgClientRect.height);
+                resolve(canvas.toDataURL("image/" + format, quality));
+            }
+        });
+        return await myPromise;
+    }
+    rasterize(document.querySelector("svg"), 1, "png").then((x) => {
+        Photopea.runScript(`app.open("${x}", null, true)`);
+    });
+});
