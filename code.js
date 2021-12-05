1e3aada7f8cc4a0e8e187b8907f8f3c08a4183c9
yikuansun <yikuansun2015@gmail.com>
2021-12-05 11:42:13 -0500

    Restart if dimensions poor

--- code.js
+++ code.js
@@ -14,6 +14,7 @@ for (var inputElem of document.querySelectorAll("input, select")) {
         var w = parseFloat((await Photopea.runScript(window.parent, "app.echoToOE(app.activeDocument.width.toString());"))[0]);
         var h = parseFloat((await Photopea.runScript(window.parent, "app.echoToOE(app.activeDocument.height.toString());"))[0]);
         document.querySelector("svg").setAttribute("viewBox", `0 0 ${w} ${h}`);
+        if (isNaN(w) || isNaN(h)) location.reload();
     }
 )();
 
